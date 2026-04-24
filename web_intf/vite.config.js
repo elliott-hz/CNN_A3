1d7f0b47591249d1cbc64921f0aece8006579ca9
elliott <elliott.cn.lee@gmail.com>
2026-04-24 18:12:34 +1000

    to support jupyterlab allowed hosts-all

--- web_intf/vite.config.js
+++ web_intf/vite.config.js
@@ -2,23 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // VERY IMPORTANT
+
+  // IMPORTANT for SageMaker proxy path
   base: './',
 
   plugins: [react()],
 
   server: {
-    // MUST use 0.0.0.0
+
+    // MUST for SageMaker
     host: '0.0.0.0',
 
     port: 5173,
 
     strictPort: true,
 
-    // IMPORTANT for SageMaker/Jupyter proxy
+    // IMPORTANT
     hmr: false,
 
+    // VERY IMPORTANT
+    allowedHosts: 'all',
+
     proxy: {
+
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
